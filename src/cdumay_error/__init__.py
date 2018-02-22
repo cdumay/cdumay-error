7e6d364de6a80f8ba3ef504b7c533f6703638ffd
Cédric Dumay <cedric.dumay@gmail.com>
2018-02-22 11:30:49 +0100

    devel: marshmallow serialization

--- src/cdumay_error/__init__.py
+++ src/cdumay_error/__init__.py
@@ -7,15 +7,25 @@
 
 """
 import traceback
+from marshmallow import Schema, fields
 
 
 class Error(Exception):
-    def __init__(self, message, code=1, extra=None):
+    def __init__(self, message, code=1, msgid=None, extra=None):
         Exception.__init__(self, code, message)
         self.message = message
         self.code = code
+        self.msgid = msgid
         self.stack = traceback.format_exc()
         self.extra = extra or dict()
 
     def __str__(self):
         return "Error {}: {}".format(self.code, self.message)
+
+
+class ErrorValidator(Schema):
+    code = fields.Integer(required=True)
+    message = fields.String(required=True)
+    msgid = fields.String()
+    extra = fields.Dict()
+    stack = fields.String()
