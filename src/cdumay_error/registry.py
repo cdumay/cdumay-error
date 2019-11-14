077726bcc1a81318f1451db037c1e3e52bb51a9d
Cédric Dumay <cedric.dumay@gmail.com>
2019-11-14 11:33:55 +0100

    feat: error factory by msgid

--- src/cdumay_error/registry.py
+++ src/cdumay_error/registry.py
@@ -10,17 +10,17 @@ from collections import OrderedDict
 
 
 class Registry(object):
-    ERRORS = list()
+    __errors = OrderedDict()
 
     @classmethod
     def register(cls, clazz):
-        if clazz not in cls.ERRORS:
-            cls.ERRORS.append(clazz)
+        if clazz.MSGID not in cls.__errors:
+            cls.__errors[clazz.MSGID] = clazz
         return clazz
 
     @classmethod
     def filter_by_status(cls, code):
-        return [x for x in cls.ERRORS if x.CODE == code]
+        return [x for x in cls.__errors.values() if x.CODE == code]
 
     @staticmethod
     def error_to_dict(clazz):
@@ -31,11 +31,17 @@ class Registry(object):
 
     @classmethod
     def to_list(cls):
-        return [cls.error_to_dict(x) for x in cls.ERRORS]
+        return [cls.error_to_dict(x) for x in cls.__errors.values()]
 
     @classmethod
     def to_dict(cls):
-        return OrderedDict([
-            (x.MSGID, cls.error_to_dict(x))
-            for x in sorted(cls.ERRORS, key=lambda x: x.MSGID)
-        ])
+        return cls.__errors
+
+    @classmethod
+    def craft_error(cls, msgid, **kwargs):
+        data = dict(msgid=msgid, **kwargs)
+        if msgid in cls.__errors.keys():
+            return cls.__errors[msgid](**data)
+        else:
+            from cdumay_error import Error
+            return Error(**data)
